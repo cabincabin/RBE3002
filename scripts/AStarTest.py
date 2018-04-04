47d1d9c070a115b0dfc714880109124bb199a3b7
Floris <florisvanrossum@gmail.com>
2018-04-04 17:03:04 -0400

    astar is possibly working

--- scripts/AStarTest.py
+++ scripts/AStarTest.py
@@ -25,7 +25,7 @@ class AStar:
 
     # Find the best path
     def findPath(self):
-        evaluated = Q.PriorityQueue()
+        evaluated = []
         notEvaluated = Q.PriorityQueue()
         notEvaluated.put((startNode.fCost, startNode))
 
@@ -43,21 +43,29 @@ class AStar:
         notEvaluated.put((newNode2.fCost, newNode2))
 
         startNode.connectedNodes = [newNode1, newNode2]
-        newNode1.connectedNodes = [newNode2, endNode]
+        newNode1.connectedNodes = [startNode, newNode2, endNode]
+        endNode.connectedNodes = [newNode1]
+        newNode2.connectedNodes = [newNode1, startNode]
+
         ###################### END TESTING CODE #################
         previousSteps = {}
 
-        # Dictionary of Gcosts
-        gCost = {} # TODO What is this?
-
+        gCost = {} # Dictionary of Gcosts
+        fCost = {} # Dictionary of F Cost
         gCost[startNode] = 0 # Cost of travelling from start node to start node is 0
 
         #fCostGraph[startNode] = self.heuristicCost(startNode) # Calculate the heuristic cost
 
         # evaluating = []
 
-        while not notEvaluated.empty():
+        pathFound = False # Toggle variable to end loop
+
+        while not notEvaluated.empty() and not pathFound:
             current = notEvaluated.get()[1]
+            current.calculateGCost(startNode)
+            current.calculateHCost(endNode)
+            current.calculateFCost()
+
             print("Node pulled: X: " + str(current.x) + " Y: " + str(current.y) + " FCost: " + str(current.fCost))
 
             if current.isSame(endNode):
@@ -65,80 +73,96 @@ class AStar:
                 # return previousSteps + Current as a path
                 print("--------------A Star Loop Ended-------------")
                 print("Path is found")
-                pass
+                pathFound = True
+                continue
 
 
-            evaluated.put((current.fCost, current))
+            evaluated.append((current))
 
             # Check the surrounding neighbors of current
 
             for neighbor in current.connectedNodes:
-                # Left Neighbor
 
-                if neighbor in current.connectedNodes:
+                print("\t\tThis is the current neighbor under investigation: X: " + str(neighbor.x) + " Y: " + str(neighbor.y) + " F: " + str(neighbor.fCost))
+
+                if neighbor in evaluated:
+                    print("\t\t\tNeighbor is already evaluated")
                     continue
 
-                tentative_gCost = gCost[current] + current.calculateDistance(neighbor)
+                tentative_gCost = gCost[current] + current.calculateMDistance(neighbor)
 
+                try:
+                    gCost[neighbor]
+                except KeyError:
+                    gCost[neighbor] = current.calculateMDistance(neighbor)
                 # Neighbor is not yet in our discovered set, add it
-                if neighbor not in notEvaluated:
+                if not self.checkIfInArray(notEvaluated, neighbor):
                     notEvaluated.put((neighbor.fCost, neighbor))
+
                 elif (tentative_gCost >= gCost[neighbor]):
-                    # This is a better path
+                    # This is NOT a better path
                     continue
 
                 previousSteps[neighbor] = current
                 gCost[neighbor] = tentative_gCost
-                fCost[neigbor] = gCost[neighbor] + self.heuristicCost(neighbor)
+                fCost[neighbor] = gCost[neighbor] + self.heuristicCost(neighbor)
 
-        print("--------------A Star Loop Ended-------------")
-        print("We have not found a path")
+        if not pathFound:
+            print("--------------A Star Loop Ended-------------")
+            print("We have not found a path")
 
     def heuristicCost(self, wayPoint):
         return math.sqrt((endNode.x - wayPoint.x) ** 2 + (endNode.y - wayPoint.y) ** 2)
 
+    def checkIfInArray(self, queue, wayPoint):
+        while not queue.empty():
+            itemGet = queue.get()
+            if(wayPoint.isSame(itemGet[1])):
+                return True
+        else:
+             return False
 
 class WayPoint:
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.hCost = 0
+        self.hCost = 0 # Euclidian distance (future cost)
         # 100 for wall
         # 0 for empty
         # -1 for unknown
         self.cost = 0
-        self.gCost = 0
-        self.fCost = 0
+        self.gCost = 0 # Historical cost
+        self.fCost = 0 # Total cost
 
         # Array containing all the connected nodes to this node
         self.connectedNodes = []
 
-    # The waypoint's historical cost
-    def calculateHCost(self, startNode):
+    # The waypoint's h cost
+    def calculateHCost(self, endNode):
         # The Manhattan Distance to the beginning
-        self.hCost = (self.x - startNode.x) + (self.y - startNode.y)
+        self.hCost = math.sqrt((endNode.x - self.x)**2 + (endNode.y - self.y)**2)
 
-    def calculateGCost(self, endNode):
-        # Euclidian Distance to the end
-        self.gCost = math.sqrt((endNode.x - self.x)**2 + (endNode.y - self.y)**2)
+    def calculateGCost(self, startNode):
+        # Historical cost
+        self.gCost = (self.x - startNode.x) + (self.y - startNode.y)
 
     def calculateFCost(self):
         self.fCost = self.gCost + self.hCost
 
     # Check if this node and compareNode are occupying the same space
     def isSame(self, compareNode):
-        if( (compareNode.x == self.x) and (compareNode.y == self.y)):
+        if((compareNode.x == self.x) and (compareNode.y == self.y)):
             return True
         else:
             return False
 
-    # Return the distance between this node and a compared node
-    def calculateDistance(self, compareNode):
-        return (math.sqrt(((self.x - compareNode.x) ** 2) + ( (self.y - compareNode.y)** 2) ))
+    # Return the Manhattan distance between this node and a compared node
+    def calculateMDistance(self, compareNode):
+        return (abs(self.x - compareNode.x) + abs(self.y - compareNode.y))
 
 if __name__ == '__main__':
-    endNode = WayPoint(100,100)
+    endNode = WayPoint(1000,1000)
     startNode = WayPoint(0,0)
     aStar = AStar(startNode,endNode)
     aStar.findPath()
